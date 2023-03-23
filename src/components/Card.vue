df0ef573638302f504b7896489a64e96e730d16a
Ryan Levick <me@ryanlevick.com>
2023-03-23 16:20:48 +0100

    Card styling

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="card" :class="{ empty: !cardValue }">
     <template v-if="cardValue">
+      <div class="mini-value top left">{{ cardValue }}</div>
+      <div class="mini-value top right">{{ cardValue }}</div>
+      <div class="mini-value bottom left">{{ cardValue }}</div>
+      <div class="mini-value bottom right">{{ cardValue }}</div>
+      <div class="card-background" :class="`background-${cardPoints}`"></div>
       <div class="card-points">
         <template v-for="i in cardPoints" :key="i">
-          <img src="../assets/cat.svg" alt="Cat Icon" class="cat-icon">
+          <img src="../assets/atom.svg" alt="Atom Icon" class="mini-icon">
         </template>
       </div>
+      <img src="../assets/atom.svg" alt="Atom Icon" class="main-icon">
       <div class="card-value">{{ cardValue }}</div>
     </template>
   </div>
@@ -30,7 +36,8 @@ export default {
 
 <style scoped>
 .card {
-  background: #C7F9CC;
+  position: relative;
+  background: whitesmoke;
   width: 100px;
   height: 150px;
   border: 1px solid black;
@@ -43,18 +50,85 @@ export default {
   font-weight: bold;
 }
 
+.card-background {
+  border-radius: 5px;
+  position: absolute;
+  width: 85%;
+  height: 90%;
+  z-index: 0;
+}
+
 .card.empty {
   border: 1px dashed black;
 }
 
 .card-value {
-  margin-bottom: 10px;
+  font-family: "Bangers";
+  z-index: 100;
+  font-size: 3rem;
+  color: #F5F4EB;
+  -webkit-text-stroke: 2px black;
 }
 
-.cat-icon {
-  width: 24px;
-  height: 24px;
-  margin-right: 4px;
+.card-points {
+  position: absolute;
+  top: 0;
+  margin-top: 5px;
+  z-index: 100;
+  width: 60%;
+}
+
+.mini-icon {
+  margin: 3px;
+  width: 14px;
+  height: 14px;
+}
+
+.mini-value {
+  font-family: "Bangers";
+  position: absolute;
+  font-size: 10px;
+  margin: 5px;
+  z-index: 50;
+}
+
+.top {
+  top: 0px;
+}
+
+.bottom {
+  bottom: 0px;
+}
+
+.right {
+  right: 0px;
+}
+
+.left {
+  left: 0px;
+}
+
+.background-2 {
+  background: #C7F9CC;
+}
+
+.background-3 {
+  background: #80ED99;
+}
+
+.background-5 {
+  background: #57CC99;
+}
+
+.background-6 {
+  background: #38A3A5;
+}
+
+.main-icon {
+  position: absolute;
+  z-index: 50;
+  width: 50px;
+  height: 50px;
 }
 </style>
 
